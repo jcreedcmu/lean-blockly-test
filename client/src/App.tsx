622f0eb6bf6f973e2e50c20390d604eebc2e58fe
Jason Reed <jcreed@gmail.com>
2026-01-25 17:51:04 -0500

    more example cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -47,25 +47,36 @@ const levelDefinitions: LevelDefinition[] = [
     initial: {
       "blocks": {
         "languageVersion": 0,
-        "blocks": [{
-          "type": "lemma",
-          "id": "test-lemma",
-          "x": 20,
-          "y": 20,
-          "fields": {
-            "THEOREM_NAME": "test",
-            "THEOREM_DECLARATION": ": 𝕜 = 𝕜"
+        "blocks": [
+          {
+            "type": "lemma",
+            "id": "test-lemma",
+            "x": 23,
+            "y": 38,
+            "fields": {
+              "THEOREM_NAME": "test",
+              "THEOREM_DECLARATION": "(x y: ℝ) (Bob : x = 2) : x + y = 2 + y"
+            }
           },
-          "inputs": {
-            "LEMMA_PROOF": {
-              "block": {
-                "type": "tactic_other",
-                "id": "test-tactic",
-                "fields": { "PROP_NAME": "rfl" }
-              }
+          {
+            "type": "prop",
+            "id": ".y:*JxJHD@%(Q8JJRL4?",
+            "x": 159,
+            "y": 224,
+            "fields": {
+              "PROP_NAME": "Bob"
+            }
+          },
+          {
+            "type": "tactic_rw",
+            "id": "z_*{MfAzbYpX8ZTwoCPY",
+            "x": 338,
+            "y": 308,
+            "fields": {
+              "DIRECTION_TYPE": "RIGHT"
             }
           }
-        }]
+        ]
       }
     }
   },
