c982ee029d3a94a485d5f6c547668d391ed3279a
Jason Reed <jcreed@gmail.com>
2026-01-25 12:46:25 -0500

    unit tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/client/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
